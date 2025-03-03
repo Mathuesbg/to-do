6ab56a5fcddae1df9d1ce79a0671900c81748ab9
Matheus <matheus.brgsz@gmail.com>
2025-03-03 12:56:30 -0300

    Add task management templates and views for creating, listing, updating, and deleting tasks

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from core import views
 
+app_name = "todo"
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('create/', views.create_task, name="create"),
+    path('status/<int:id>', views.change_status, name="status"),
+    path('delete/<int:id>', views.delete_task, name="delete"),
+    path('detail/<int:id>', views.detail_task, name="detail"),
+    path('update/<int:id>', views.update_task, name="update"),
+    path('', views.list_task, name="list"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,84 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from core.forms import CreateTaskForm
+from django.contrib import messages
+from core.models import Task
+from django.utils import timezone
 
 # Create your views here.
-def index(request):
-    return HttpResponse("oi")
+def list_task(request):
+    tasks = Task.objects.all().order_by('-id')
+    context = {
+        "tasks" : tasks
+    }
+
+    return render(
+        request=request,
+        template_name= "todo/list_task.html",
+        context=context
+        )
+
+def create_task(request):
+
+    if request.method == "POST":
+        form = CreateTaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request=request, message="Task created sucessfully")
+            return redirect('todo:create')
+    else:
+        form = CreateTaskForm()
+
+    context = {"form": form}
+    return render(
+        request=request,
+        template_name="todo/create_form.html",
+        context=context
+        )
+
+
+def change_status(request, id):
+
+    task= get_object_or_404(klass=Task, pk=id)
+    task.status = "completed"
+    task.finished_at = timezone.now()
+    task.save()
+    return redirect('todo:list')
+
+def delete_task(request,id):
+    task= get_object_or_404(klass=Task, pk=id)
+    task.delete()
+    return redirect('todo:list')
+
+
+def detail_task(request, id):
+    task= get_object_or_404(klass=Task, pk=id)
+    context = {
+        "task" : task
+    }
+
+    return render(
+        request=request,
+        template_name= "todo/detail_task.html",
+        context=context
+        )
+
+def update_task(request, id):
+
+    task= get_object_or_404(klass=Task, pk=id)
+
+    if request.method == "POST":
+        form = CreateTaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request=request, message="Task updated sucessfully")
+            return redirect('todo:create')
+    else:
+        form = CreateTaskForm(instance=task)
+
+    context = {"form": form,
+               "task": task}
+    return render(
+        request=request,
+        template_name="todo/update_task.html",
+        context=context
+        )
